96df809bc393d757bc146aef02211aa4566b0639
shriyansh <shriyanshneema27@gmail.com>
2020-03-31 11:00:55 -0700

    add devise gem for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  devise_for :users
+  # root to: 'home#index'
   resources :portfolios
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'homes', to: 'home#index'
+  
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
